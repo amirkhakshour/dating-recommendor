d008b898bcc828c10516b35a5571b608a410259f
amir <amirkhakshour@gmail.com>
2020-09-05 19:35:15 +0200

    added flask app file

---
+++ main.py
@@ -0,0 +1,8 @@
+from flask import Flask
+from flask_restful import Api
+
+app = Flask(__name__)
+api = Api(app, catch_all_404s=True)
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
